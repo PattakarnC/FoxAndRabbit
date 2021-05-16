a6f538292a15cca9060d4e42cb936a313016a92e
Pattakarn <pattakarn.c@gmail.com>
2021-05-17 00:02:50 +0700

    Class created

---
+++ src/main/java/io/muic/ssc/FoxAndRabbit/Main.java
@@ -0,0 +1,8 @@
+package io.muic.ssc.FoxAndRabbit;
+
+public class Main {
+    public static void main(String[] args) {
+        var simulator = new Simulator();
+        simulator.simulate(1000);
+    }
+}
